6470b1ff0d7dacd58cf99b710103f9a0137d1cb1
Claudio Capobianco <wbigger@gmail.com>
2017-09-26 05:45:31 +0200

    Add a simple, almost empty component

---
+++ src/app/menu-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-menu-list',
+    templateUrl: './menu-list.component.html' 
+})
+
+export class MenuListComponent {
+    pageTitle = 'Mensa INPS'
+}
