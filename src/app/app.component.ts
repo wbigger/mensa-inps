2410f92ac3f51dcd4e54d92ee5efd3dcc9c70e84
Claudio Capobianco <wbigger@gmail.com>
2017-09-27 12:07:12 +0200

    Add service to get dishes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { DishService } from './dish.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DishService]
 })
 export class AppComponent {
   title = 'app';
