564b3ee1256612ded1ac51e394b9ab8b7ba3037e
Claudio Capobianco <wbigger@gmail.com>
2017-09-27 09:23:22 +0200

    Update test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('mensa-inps App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Menù della giornata');
   });
 });
