a032dc549fc4e9e6bdc138755af0a34e0cd06fa9
Claudio Capobianco <wbigger@gmail.com>
2017-09-27 05:30:45 +0200

    Show list of dishes
    Close user story #4

---
+++ src/app/dish.ts
@@ -0,0 +1,8 @@
+export interface Dish {
+    course: string
+    name: string
+    price: number
+    imageUrl: string
+    diet: string
+    counter: number
+}
--- src/app/menu-list.component.ts
+++ src/app/menu-list.component.ts
@@ -1,11 +1,50 @@
 import { Component } from "@angular/core";
+import { Dish } from "./dish";
 
 
 @Component({
     selector: 'app-menu-list',
-    templateUrl: './menu-list.component.html' 
+    templateUrl: './menu-list.component.html',
+    styleUrls: ['./menu-list.component.css']
 })
 
-export class MenuListComponent {
+export class MenuListComponent  {
     panelHeading = 'Menù della giornata'
+    imageWidth = 100;
+    imageMargin = 5;
+    dishes: Dish[] = [
+        {
+            'course': 'Primo',
+            'name': 'Carbonara',
+            'price': 6.5,
+            'imageUrl': 'http://img.taste.com.au/86bOXAkG/taste/2016/11/carbonara-sauce-28894-1.jpeg',
+            'counter': 0,
+            'diet': 'pork'
+        },
+        {
+            'course': 'Primo',
+            'name': 'Cacio & Pepe',
+            'price': 6.5,
+            'imageUrl': 'https://cms.splendidtable.org/sites/default/files/styles/w2000/public/Cacio-e-Pepe_TASTING-ROME.jpg',
+            'counter': 0,
+            'diet': 'vegetarian'
+        },
+        {
+            'course': 'Secondo',
+            'name': 'Platessa',
+            'price': 5,
+            'imageUrl': 'http://www.buonissimo.org/archive/borg/ivbJTo6zUXMeIkTw3%252FB%252BOe%252FYr971i3%252BH%252BDl3jE%252FTTk6gnY0PsxvV6g%253D%253D',
+            'counter': 0,
+            'diet': ''
+        },
+        {
+            'course': 'Contorno',
+            'name': 'Patate fritte',
+            'price': 3,
+            'imageUrl': 'http://www.giallozafferano.it/images/ricette/8/896/foto_hd/hd292x195.jpg',
+            'counter': 1,
+            'diet': 'vegetarian'
+        },
+    ]
+
 }
