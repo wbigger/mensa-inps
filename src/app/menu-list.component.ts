df186b1251bc972e6270d03c08f94a0836dada1c
Claudio Capobianco <wbigger@gmail.com>
2017-09-27 05:46:53 +0200

    Add order functionality
    Now just print on console.
    Close story #1.

--- src/app/menu-list.component.ts
+++ src/app/menu-list.component.ts
@@ -12,6 +12,7 @@ export class MenuListComponent  {
     panelHeading = 'Menù della giornata'
     imageWidth = 100;
     imageMargin = 5;
+    dishCounter = 0;
     dishes: Dish[] = [
         {
             'course': 'Primo',
@@ -47,4 +48,52 @@ export class MenuListComponent  {
         },
     ]
 
+    // Create a property with getter and setter
+    // This property with underscore will store the data internally
+    _totPrice:number = 0
+    // This function will get the value of the property
+    get totPrice(): number {
+        this._totPrice = 0
+        this.dishes.forEach(x => this._totPrice += (x.counter * x.price))
+        return this._totPrice;
+    }
+    // This function will set the value of the property
+    set totPrice(value:number){
+        this._totPrice = value
+    }
+
+    incCounter(d: Dish) {
+        //this.dishes[0].counter = +this.dishes[0].counter + 1
+        d.counter = +d.counter + 1
+    }
+
+    decCounter(d: Dish) {
+        if (d.counter > 0) {
+            d.counter = +d.counter - 1
+        }
+    }
+
+    reset() {
+        // Here we use the 'classic' for loop
+        for (let dish of this.dishes) {
+            if (dish.counter > 0) {
+                console.log(`Hai ordinato ${dish.counter} ${(dish.counter==1)?'porzione':'porzioni'} di ${dish.name}`)
+            }
+        }
+    }
+
+    doOrder() {
+        // Here we use arrow functions.
+
+        // Filter the elements and print the log
+        this.dishes
+        .filter(dish => dish.counter > 0)
+        .forEach(dish => console.log(`Hai ordinato ${dish.counter} ${(dish.counter==1)?'porzione':'porzioni'} di ${dish.name}`))
+
+        // Sum all the prices.
+        // As you can see, the arrow function can access external vars
+        let totPrice = 0
+        this.dishes.forEach(x => totPrice += (x.counter * x.price))
+        console.log('Hai speso un totale di '+ totPrice + ' euro')
+    }
 }
